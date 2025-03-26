629af30c3303723ab3c960e5d8444a48fa3e5708
JuniorD-Isael <isaeldjunior@gmail.com>
2025-03-26 19:43:22 -0300

    Instalado as dependências flask-cors e gunicorn para deploy no render

--- src/app.py
+++ src/app.py
@@ -3,8 +3,11 @@ import sqlite3
 import re
 import os
 import logging
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 db_path = os.getenv("DB_PATH", "database.db")
 logging.basicConfig(level=logging.INFO)
